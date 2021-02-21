fa06ed730639aeb7fb47c327834b184d07ebd52f
Trevin Hofmann <trevinhofmann@gmail.com>
2021-02-20 14:18:03 -1000

    refactor(lint): applying minor whitespace, comma, and semicolon fixes

--- src/components/jest/index.ts
+++ src/components/jest/index.ts
@@ -1,12 +1,12 @@
-module.exports =  {
-    plugins: [
-      'jest',
-    ],
-    extends: [
-      './best-practices',
-      './possible-errors',
-    ].map(path => require.resolve(path)),
-    // env: {
-    //   'jest/globals': true,
-    // }, // TODO
-  }
+module.exports = {
+  plugins: [
+    'jest',
+  ],
+  extends: [
+    './best-practices',
+    './possible-errors',
+  ].map(path => require.resolve(path)),
+  // env: {
+  //   'jest/globals': true,
+  // }, // TODO
+};
